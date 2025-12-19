d739e07282ed8260ef0252d36839056883ae7eb8
Akshit <akshit07032001@gmail.com>
2025-12-19 17:36:01 +0530

    init: mongodb connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+import connectDB from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+connectDB();
+
 app.use(cors());
 app.use(express.json());
+
+app.get('/health', (req, res) => {
+  res.json({ message: 'Server running!' });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
