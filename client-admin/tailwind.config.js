9c4d26a7bf03a2b90286d762bb411795b9883adf
Akshit <akshit07032001@gmail.com>
2025-12-20 18:58:57 +0530

    commit

---
+++ client-admin/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
